3de4a4ad9b90aeef46e453e68eb0fb01eaa08712
Harrison Holgate <harrisonrobertholgate@gmail.c>
2025-01-28 17:05:30 +0000

    Completed exercise 14.

--- practical_14/practical_14.srcs/sources_1/new/combination_lock.vhd
+++ practical_14/practical_14.srcs/sources_1/new/combination_lock.vhd
@@ -4,62 +4,81 @@ use ieee.numeric_std.all;
 
 entity combination_lock is
     port (
-        clk        : in std_logic;
-        clear_in   : in std_logic;
-        buttons_in : in std_logic_vector(3 downto 0);
-        locked_out : out std_logic;
-        state_out  : out std_logic_vector(3 downto 0) := (others => '0')
+        clk                 : in std_logic;
+        --
+        reset_in            : in std_logic;
+        enter_in            : in std_logic;
+        code_in             : in std_logic_vector(7 downto 0);
+        --
+        locked_out          : out std_logic;
+        timeout_out         : out std_logic := '0';
+        state_out           : out std_logic_vector(3 downto 0) := (others => '0');
+        cnt_out             : out std_logic_vector(30 downto 0)
     );
 end entity;
 
 architecture rtl of combination_lock is
-    type state_t is (locked, code1, code2, code3, unlocked);
-    signal state_r : state_t := locked;
+    type state_t is (attempt_1, attempt_2, attempt_3, unlocked, timeout);
+    signal state_r : state_t := attempt_1;
     constant passcode : std_logic_vector(7 downto 0) := "00011011"; -- 8-bit passcode: button 1, button 2, button 3, button 4.
+    constant TIMEOUT_CLKS : unsigned(30 downto 0) := (
+        others       => '1'   -- Remaining 29 bits set low
+    );
+    signal cnt : unsigned(30 downto 0) := (others => '0');
 begin
+    cnt_out <= std_logic_vector(cnt);
+
     fsm_proc : process(clk) is
     begin
         if rising_edge(clk) then
-            if clear_in = '1' then
-                state_r <= locked; -- Reset to locked state
-                state_out <= "0000";
-            else
-                case state_r is
-                    when locked =>
+            case state_r is
+                when attempt_1 =>
+                    locked_out <= '1';
+                    state_out <= "0001";
+                    if (enter_in = '1') then
+                        if (code_in = passcode) then
+                            state_r   <= unlocked;
+                        else
+                            state_r   <= attempt_2; 
+                        end if;
+                    end if;
+                when attempt_2 =>
                         locked_out <= '1';
-                        if buttons_in(to_integer(unsigned(passcode(7 downto 6)))) = '1' then
-                            state_r <= code1;
-                            state_out <= "0001";
+                        state_out <= "0011";
+                        if (enter_in = '1') then
+                            if (code_in = passcode) then
+                                state_r   <= unlocked;
+                            else
+                                state_r   <= attempt_3; 
+                            end if;
                         end if;
-                    when code1 =>
+                when attempt_3 =>
                         locked_out <= '1';
-                        if buttons_in(to_integer(unsigned(passcode(5 downto 4)))) = '1' then
-                            state_r <= code2;
-                            state_out <= "0011";
+                        state_out <= "0111";
+                        if (enter_in = '1') then
+                            if (code_in = passcode) then
+                                state_r   <= unlocked;
+                            else
+                                state_r   <= timeout; 
+                            end if;
                         end if;
-
-                    when code2 =>
-                        locked_out <= '1';
-                        if buttons_in(to_integer(unsigned(passcode(3 downto 2)))) = '1' then
-                            state_r <= code3;
-                            state_out <= "0111";
-                        end if;
-
-                    when code3 =>
-                        locked_out <= '1';
-                        if buttons_in(to_integer(unsigned(passcode(1 downto 0)))) = '1' then
-                            state_r <= unlocked;
-                            state_out <= "1111";
-                        end if;
-
-                    when unlocked =>
-                        locked_out <= '0';
-                        if clear_in = '1' then
-                            state_r <= locked;
-                            state_out <= "0000";
-                        end if;
+                when timeout =>
+                    locked_out <= '1';
+                    timeout_out <= '1';
+                    if cnt < TIMEOUT_CLKS then
+                        cnt <= cnt + 1;
+                    else
+                        cnt <= (others => '0');
+                        timeout_out <= '0';
+                        state_r <= attempt_1;
+                    end if;
+                when unlocked =>
+                    locked_out <= '0';
+                    state_out <= "0000";
+                    if reset_in = '1' then
+                        state_r <= attempt_1;
+                    end if;                                                       
                 end case;
-            end if;
         end if;
     end process;
 end architecture;
--- practical_14/practical_14.srcs/sources_1/new/top_level.vhd
+++ practical_14/practical_14.srcs/sources_1/new/top_level.vhd
@@ -11,36 +11,63 @@ entity toplevel is
     --
     buttons                 : in std_logic_vector(4 downto 0);
     --
-    leds                    : out std_logic_vector(15 downto 0)
+    leds                    : out std_logic_vector(15 downto 0);
+    -- 
+    seg                     : out std_logic_vector(6 downto 0)
     );
 end entity;
 
 architecture rtl of toplevel is
-    signal fifo_write, fifo_read : std_logic;
-    signal buttons_dbn           : std_logic_vector(4 downto 0);
+    signal fifo_write, fifo_read                : std_logic;
+    signal buttons_dbn, buttons_dbn_r           : std_logic_vector(1 downto 0);
+    signal timeout                              : std_logic;
+    signal comb_enter, comb_clear               : std_logic := '0';
+    signal cnt                                  : std_logic_vector(30 downto 0);
 begin
 
+    -- Instantiate debounce component for each button.
     buttons_0_i : entity work.debounce
-    port map (clk => clk, bit_in => buttons(0), bit_out => buttons_dbn(0));
+    port map (clk => clk, bit_in => buttons(2), bit_out => buttons_dbn(0));
     buttons_1_i : entity work.debounce
-    port map (clk => clk, bit_in => buttons(1), bit_out => buttons_dbn(1));
-    buttons_2_i : entity work.debounce
-    port map (clk => clk, bit_in => buttons(2), bit_out => buttons_dbn(2));
-    buttons_3_i : entity work.debounce
-    port map (clk => clk, bit_in => buttons(3), bit_out => buttons_dbn(3));
-    buttons_4_i : entity work.debounce
-    port map (clk => clk, bit_in => buttons(4), bit_out => buttons_dbn(4));
+    port map (clk => clk, bit_in => buttons(3), bit_out => buttons_dbn(1));
+    
+    button_edge_proc : process(clk) is
+    begin
+        if rising_edge(clk) then
+           for i in buttons_dbn_r'range loop
+               buttons_dbn_r(i) <= buttons_dbn(i);
+           end loop;
+        end if;
+    end process;
 
+    comb_enter <= buttons_dbn(0) and not buttons_dbn_r(0);
+    comb_clear <= buttons_dbn(1) and not buttons_dbn_r(1);
+    
     fifo_i : entity work.combination_lock
     port map
     (
         clk                 => clk,
         --
-        clear_in            => buttons_dbn(0),
-        buttons_in          => buttons_dbn(4 downto 1),
+        reset_in            => comb_clear,
+        enter_in            => comb_enter,
+        code_in             => switches(15 downto 8),
         --
         locked_out          => leds(0),
-        state_out           => leds(4 downto 1)
+        state_out           => leds(4 downto 1),
+        timeout_out         => timeout,
+        cnt_out             => cnt
      );
-
+     
+     
+     timeout_proc : process(clk) is
+     begin
+        if rising_edge(clk) then
+            if timeout = '1' then
+                leds(15 downto 5) <= cnt(30 downto 20);
+                seg(6 downto 0) <= cnt(30 downto 24);
+            else
+                leds(15 downto 5) <= (others => '0');
+            end if;
+        end if;
+     end process;
 end architecture;
